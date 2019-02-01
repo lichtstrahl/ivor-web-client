5eab7e0514480a1704be3bbad9d672bcd0489003
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2019-02-01 21:09:26 +0300

    Направил на новый сервер ivor-java-server

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const BASE_URL :string =  "https://pure-headland-78190.herokuapp.com";
+export const BASE_URL :string =  "https://ivor-java-server.herokuapp.com";
 export enum MainViewState {
     ACTIVITY_LOGIN,
     ACTIVITY_REG,
