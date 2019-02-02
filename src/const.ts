6556e8e2bd2066f795e2397d75070fecb39b66bf
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2019-02-02 11:53:42 +0300

    Подправил константы

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,6 @@
-export const BASE_URL :string =  "https://ivor-java-server.herokuapp.com";
+const IVOR_JAVA_SERVER:string = "https://ivor-java-server.herokuapp.com";
+const LOCAL_SERVER:string = "http://localhost:8080";
+export const BASE_URL :string =  IVOR_JAVA_SERVER;
 export enum MainViewState {
     ACTIVITY_LOGIN,
     ACTIVITY_REG,
