868ec6d8d128148179e1d3984072433f08f08321
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2019-03-13 16:10:40 +0300

    Добавил redux в LoginView

--- src/components/LoginView.tsx
+++ src/components/LoginView.tsx
@@ -1,40 +1,151 @@
+// import Label from "./primitive/Label";
+// import * as React from 'react'
+// import * as Redux from 'redux';
+// import {Button} from "react-bootstrap";
+// import {Link} from "react-router-dom";
+//
+// import {connect} from "react-redux";
+// import axios, {AxiosResponse} from "axios";
+// import {BASE_URL} from "../const";
+// import {store} from "../index";
+// import {func} from "prop-types";
+// import {Action} from "redux";
+//
+// interface Props extends Action {
+//     count:number
+// }
+//
+// interface State {
+//     currentState:string
+// }
+//
+// class LoginView extends React.Component<Props, State> {
+//     private successfulLogin :boolean;
+//     private inputLogin      :string;
+//     private inputPassword   :string;
+//
+//     constructor(props: Props) {
+//         super(props);
+//         this.successfulLogin = false;
+//         this.inputLogin = "";
+//         this.inputPassword = "";
+//         console.log(this);
+//         this.clickLogin.bind(this);
+//         this.changeLoginText.bind(this);
+//         this.changePasswordText.bind(this);
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <Label text={"Вход"}/>
+//                 <form>
+//                     <label>
+//                         Login:
+//                         <input type={"text"} name={"inputLogin"} placeholder={"логин"} onChange={this.changeLoginText.bind(this)} />
+//                     </label>
+//                     <br/>
+//                     <label>
+//                         Password:
+//                         <input type={"password"} name={"inputPass"} placeholder={"Пароль"} onChange={this.changePasswordText.bind(this)}/>
+//                     </label>
+//                     <br/>
+//                     <Button variant={"outline-primary"} onClick={this.clickLogin.bind(this)}>Войти</Button>
+//                 </form>
+//                 <Link to={'/register'}><Button variant={"outline-secondary"}>Регистрация</Button></Link>
+//             </div>
+//         )
+//     }
+//
+//     clickLogin = (event:any) => {
+//         axios.post(BASE_URL + "/api/login", {login:this.inputLogin, pass:this.inputPassword})
+//             .then((res:AxiosResponse) => {
+//                 let data = res.data;
+//                 console.log(data);
+//                 // data.error == 0 ? (this.successfulLoginCallback(data.data)) : (this.failedLoginCallback(data.msg));
+//
+//             });
+//         event.preventDefault(); // Чтобы избежать перезагрузки страницы
+//     };
+//
+//     changeLoginText = (event:any) => {
+//         this.inputLogin = event.target.value;
+//     };
+//
+//     changePasswordText = (event:any) => {
+//         this.inputPassword = event.target.value;
+//         // this.setState({password: event.target.value})
+//     };
+// }
+//
+// function mapDispatchToProps(dispatch: Redux.Dispatch<Props>) {
+//     return dispatch({type: "A_!", count: 0});
+// }
+//
+// function mapStateToProps(state:State) {
+//     return {label: "CurrentState: " + state.currentState};
+// }
+//
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+// )(LoginView);
 
-import Login from "./forms/Login";
-import Label from "./primitive/Label";
-import * as React from 'react'
-import {Button} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import * as React from "react";
+import * as Redux from "react-redux";
 
-type LoginProps = {
-    successfulLogin     : () => {},
-    failedLogin          : (msg:string) => {}
+type PropsStore = {
+    globalStore:any
 }
 
-export default class LoginView extends React.Component<any, any> {
-    private readonly failedLoginCallback        : (msg:string) => {};
-    private readonly successfulLoginCallback    : ()=>{};
-    private successfulLogin:boolean;
+type PropsDispatch = {
+    onAddTrack: (a:string) => void
+}
 
-    constructor(props: LoginProps) {
-        super(props);
-        this.failedLoginCallback = props.failedLogin;
-        this.successfulLoginCallback = props.successfulLogin;
-        this.successfulLogin = false;
-        console.log(this);
-    }
+type Props = PropsStore & PropsDispatch;
 
-    render() {
+class LoginView extends React.Component<Props, any> {
+    private inputTrack:HTMLInputElement|null = null;
+
+    render(): React.ReactNode {
+        console.log("Render login view: ",this.props.globalStore);
         return (
-            <div>
-                <Label text={"Вход"}/>
-                <Login successfulLogin={this.successfulLoginCallback} failedLogin={this.failedLoginCallback}/>
-                <Link to={'/register'}><Button variant={"outline-secondary"}>Регистрация</Button></Link>
-            </div>
-        )
+          <div>
+              <input type={"text"} ref={(input) => {this.inputTrack = input}}/>
+              <button onClick={this.addTrack.bind(this)}>Add track</button>
+              <ul>
+                  {this.props.globalStore.map((track:any, i:number) => <li key={i}>{track}</li>)}
+              </ul>
+          </div>  
+        );
     }
 
-    // successfulLoginCallback = () => {
-    //     this.successfulLogin = true;
-    // }
-
+    addTrack() {
+        const input:string = this.inputTrack!.value;
+        if (input.length != 0) {
+            console.log("add track", this.inputTrack!.value);
+            this.props.onAddTrack(this.inputTrack!.value);
+            this.inputTrack!.value = '';
+        }
+    }
 }
+
+// Передаётся две функции.
+// 1 - Мапит state из Стора в Props React-компонента.
+// 2 -
+export default Redux.connect(
+    state =>  {
+        let prp:PropsStore = {
+            globalStore:state
+        };
+        return prp;
+    },
+    dispatch => {
+        let prp:PropsDispatch = {
+          onAddTrack: (trackName:string) => {
+              dispatch({type: "ADD", name: trackName})
+          }
+        };
+        return prp;
+    }
+)(LoginView);
--- src/index.js
+++ src/index.js
@@ -1,99 +1,149 @@
-// import React from 'react'
+import React from 'react'
 import {render} from 'react-dom'
-import {BrowserRouter, Route, Router} from "react-router-dom"
-// import LoginView from "./components/LoginView";
-// import RegView from "./components/RegView";
-// import MsgView from "./components/MsgView";
-// import {combineReducers, createStore} from "redux"
-// import {Provider} from "react-redux"
-// import reducers from '<project-path>/reducers'
-// import {routerReducer} from "react-router-redux/src";
-//
-// const store = createStore(
-//     combineReducers({
-//         ...reducers,
-//         routing: routerReducer
-//     })
-// );
-//
-// render((
-//     <Provider store={store}>
-//         <BrowserRouter>
-//             <div>
-//                 <Route exact path={"/"} component={LoginView} />
-//                 <Route exact path={"/register"} component={RegView} />
-//                 <Route exact path={"/msg"} component={MsgView} />
-//             </div>
-//         </BrowserRouter>
-//     </Provider>
-// ), document.getElementById("root"));
+import {BrowserRouter, Route} from "react-router-dom"
+import LoginView from "./components/LoginView";
+import RegView from "./components/RegView";
+import MsgView from "./components/MsgView";
+import {createStore} from "redux"
+import {Provider} from "react-redux"
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
 
-const initState = {count:0};
 
-function reducer(state = {count: 0}, action) {
+const initState = [
+    'Track 1'
+];
+
+
+
+// Данные неизменные
+export function updateStore(state, action) {
     switch (action.type) {
-        case "INC":
-            return {count: state.count + action.arg};
-
-        case "DEC":
-            return {count: state.count - action.arg};
-
-        case "RESET":
-            return {count: 0};
+        case "ADD":
+            return [...state, action.name]; // Добавляем новое значение в массив и возвращаем новый массив
         default:
             return state;
     }
 }
+// Передаётся reducer и начальное состояние
+export const store = createStore(updateStore, initState);
 
-const incA =    {type: "INC",   arg: 1};
-const decA =    {type: "DEC",   arg: 1};
-const resetA =  {type: "RESET", arg: 0};
-
-const store = createStore(reducer);
-
-class Counter extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-        this.reset = this.reset.bind(this);
-    }
-
-
-    componentDidMount() {
-        store.subscribe(() => this.forceUpdate());
-    }
-
-    increment() {
-        store.dispatch(incA);
-    }
-
-    decrement() {
-        store.dispatch(decA);
-    }
-
-    reset() {
-        store.dispatch(resetA);
-    }
-
-    render() {
-        return (
-            <div className={"counter"}>
-                <span className={"count"}>{store.getState().count}</span>
-
-                <div className={"buttons"}>
-                    <button className={"decrement"} onClick={this.decrement}>-</button>
-                    <button className={"increment"} onClick={this.increment}>+</button>
-                    <button className={"reset"} onClick={this.reset}>reset</button>
-                </div>
+render((
+    <Provider store={store}>
+        <BrowserRouter>
+            <div>
+                <Route exact path={"/"} component={LoginView} />
+                <Route exact path={"/register"} component={RegView} />
+                <Route exact path={"/msg"} component={MsgView} />
             </div>
-        );
-    }
-}
+        </BrowserRouter>
+    </Provider>
+), document.getElementById("root"));
 
-ReactDOM.render(<Counter/>, document.getElementById("root"));
+
+
+
+// import {createStore} from 'redux';
+//
+// const initState = [];
+// // Данные неизменные
+// function updateStore(state, action) {
+//     switch (action.type) {
+//         case "ADD":
+//             return [...state, action.name]; // Добавляем новое значение в массив и возвращаем новый массив
+//         default:
+//             return state;
+//     }
+// }
+//
+// const store = createStore(updateStore, initState);
+// store.subscribe(() => {
+//    console.log("Store update:", store.getState());
+// });
+//
+// store.dispatch({type: "ADD", name: "Smells like spirit"});
+// store.dispatch({type: "ADD", name: "Enter Sandman"});
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {createStore} from 'redux';
+//
+// const initState = {count:0};
+//
+// function reducer(state = {count: 0}, action) {
+//     switch (action.type) {
+//         case "INC":
+//             return {count: state.count + action.arg};
+//
+//         case "DEC":
+//             return {count: state.count - action.arg};
+//
+//         case "RESET":
+//             return {count: 0};
+//         default:
+//             return state;
+//     }
+// }
+//
+// function incrementA(arg) {
+//     return {type: "INC",   arg: arg};
+// }
+//
+// function decrementA(arg) {
+//     return {type: "DEC",   arg: arg};
+// }
+//
+// function resetA() {
+//     return {type: "RESET", arg: 0};
+// }
+//
+// const store = createStore(reducer);
+//
+// class Counter extends React.Component {
+//     constructor(props) {
+//         super(props);
+//
+//         this.increment = this.increment.bind(this);
+//         this.decrement = this.decrement.bind(this);
+//         this.reset = this.reset.bind(this);
+//     }
+//
+//
+//     componentDidMount() {
+//         store.subscribe(() => this.forceUpdate());
+//     }
+//
+//     increment() {
+//         let arg = parseInt(this.refs.arg.value || 1);
+//         store.dispatch(incrementA(arg));
+//     }
+//
+//     decrement() {
+//         let arg = parseInt(this.refs.arg.value || 1);
+//         store.dispatch(decrementA(arg));
+//     }
+//
+//     reset() {
+//         store.dispatch(resetA());
+//     }
+//
+//     render() {
+//         return (
+//             <div className={"counter"}>
+//                 <span className={"count"}>{store.getState().count}</span>
+//
+//                 <div className={"buttons"}>
+//                     <button className={"decrement"} onClick={this.decrement}>-</button>
+//                     <button className={"increment"} onClick={this.increment}>+</button>
+//                     <button className={"reset"} onClick={this.reset}>reset</button>
+//                 </div>
+//
+//                 <input type={"text"} ref={"arg"} defaultValue={"1"}/>
+//             </div>
+//         );
+//     }
+// }
+//
+// ReactDOM.render(<Counter/>, document.getElementById("root"));
